23e0aa5eeed9907e8914596450b5106aac07255e
Carlos Flores <cafg10@gmail.com>
2016-04-25 09:32:46 -0600

    Add generator column to Cotizacion

--- bridge/models.py
+++ bridge/models.py
@@ -512,6 +512,7 @@ class Cotizacion(models.Model):
     alternate = models.IntegerField()
     normal = models.IntegerField()
     ordering = models.CharField(max_length=45, blank=True, null=True)
+    generator = models.CharField(max_length=45, blank=True, null=True)
 
     class Meta:
         managed = False
